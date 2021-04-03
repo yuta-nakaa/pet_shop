e1ef3ef3a1722437da923481f683e4ee6aa708f2
yuta-nakaa <nakshimashelter55@gmail.com>
2021-04-03 20:57:50 +0900

    ペット紹介4

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-;<?php
-
-    require_once __DIR__ . '/functions.php';
-
-    $dbh = connectDb();
-
-    ?>
-
 <?php
 
 require_once __DIR__ . '/functions.php';
@@ -18,7 +10,7 @@ $stmt = $dbh->prepare($sql);
 
 $stmt->execute();
 
-$users = $stmt->fetchall(PDO::FETCH_ASSOC);
+$animals = $stmt->fetchall(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -32,7 +24,7 @@ $users = $stmt->fetchall(PDO::FETCH_ASSOC);
 <body>
     <h2>本日の動物ご紹介！</h2>
         <?php foreach ($animals as $animal) : ?>
-            <?= h($animal['type']) . 'の',  h($animal['classification'] . 'ちゃん') ?><br>
+            <?= h($animal['type']) . 'の' . h($animal['classification'] . 'ちゃん') ?><br>
             <?= h($animal['description'])  ?><br>
             <?= h($animal['birthday']) . '生まれ' ?><br>
             <?= h('出身地', $animal['birthplace'])   ?><br>
